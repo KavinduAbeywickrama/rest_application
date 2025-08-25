524d953a2180ee8383de0f03b4e92d03cd9b365a
KavinduAbeywickrama <madhura-se19054@stu.kln.ac.lk>
2025-08-25 08:57:04 +0530

    feat: initiate main files

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param, Post, Body, Patch, Delete, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+    @Get()
+        findAll(){
+            return []
+    }
+
+    @Get(':id')
+        findOne(@Param('id') id:string){
+            return { id }
+    }
+
+    @Post()
+    create(@Body() user: object){
+        return user
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string, @Body() userUpdate: {}){
+        return { id, ...userUpdate }
+    }
+    
+    @Delete(':id')
+        delete(@Param('id') id:string){
+            return { id }
+    }
+
+    @Get()
+    find(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'MANAGER'){
+            return []
+    }
+}
